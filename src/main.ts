40c86a4aae29798f8938ac9da2ccf611f4eed44b
Jaime López <jaime.e.lopez@oracle.com>
2026-03-04 15:18:47 -0600

    feat: added new BackScreen enum option

--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,8 @@ export enum BackScreen {
     Default = "default",
     OpenForm = "open_form",
     RestoreFormDraft = "restore_form_draft",
-    OpenFormSubmit = "open_form_submit"
+    OpenFormSubmit = "open_form_submit",
+    PluginByLabel = "plugin_by_label"
 }
 
 export enum Procedure {
